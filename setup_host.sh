176614cb683c6bc272606fc5b27ef3ef1fb24f17
shivani <shivani@gmail.com>
2026-05-27 09:05:12 +0000

    script file

---
+++ setup_host.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "updating packages"
+sudo apt update -y
+
+echo "Installing git"
+sudo apt install git -y
+
+echo "Installing Docker"
+# Add Docker's official GPG key:
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "start docker"
+sudo systemctl start docker
+sudo systemctl enable docker
+
+if docker --version && git --version
+then
+   echo "git and docker installed successfully"
+   sudo systemctl status docker 
+else
+   echo "Not install"
+fi
