5ff53353e38c2d049c8459c96b2d29927ab5bafc
shivani <shivani@gmail.com>
2026-05-29 16:26:24 +0000

    docker-file

--- setup_host.sh
+++ setup_host.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
-
+set -ex
 echo "updating packages"
-sudo apt update -y
+apt update -y
 
 echo "Installing git"
-sudo apt install git -y
+apt install git -y
 
 echo "Installing Docker"
 # Add Docker's official GPG key:
-sudo apt install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-Components: stable
-Architectures: $(dpkg --print-architecture)
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu jammy stable" \
+  > /etc/apt/sources.list.d/docker.list
 
-sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "start docker"
-sudo systemctl start docker
-sudo systemctl enable docker
+systemctl start docker 
+systemctl enable docker
 
 if docker --version && git --version
 then
    echo "git and docker installed successfully"
-   sudo systemctl status docker 
+   systemctl status docker 
 else
    echo "Not install"
 fi
